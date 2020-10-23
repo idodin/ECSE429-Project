805b3a248b267ff90f147c91470761884c57889a
Imad Dodin <imad.dodin@mail.mcgill.ca>
2020-10-23 19:53:49 -0400

    Clean Repository

--- challenger/src/main/java/uk/co/compendiumdev/challenge/persistence/PersistenceMechanism.java
+++
@@ -1,9 +0,0 @@
-package uk.co.compendiumdev.challenge.persistence;
-
-import uk.co.compendiumdev.challenge.ChallengerAuthData;
-
-public interface PersistenceMechanism {
-
-    PersistenceResponse saveChallengerStatus(ChallengerAuthData data);
-    PersistenceResponse loadChallengerStatus(String guid);
-}
--- challengerAuto/src/test/java/uk/co/compendiumdev/challenger/payloads/Challenges.java
+++
@@ -1,9 +0,0 @@
-package uk.co.compendiumdev.challenger.payloads;
-
-import uk.co.compendiumdev.challenger.payloads.Challenge;
-
-import java.util.List;
-
-public class Challenges {
-    public List<Challenge> challenges;
-}
